96f3d6ef16f3aec95280222ebd9970b5a4c93a9f
tdhillon-552 <taz.dhillon@gmail.com>
2022-04-04 12:55:30 -0700

    fixed template tag for custom fields.

--- log/templatetags/getlabel.py
+++ log/templatetags/getlabel.py
@@ -7,11 +7,7 @@ register = template.Library()
 
 def label(value, arg):
     if arg == "bolo":
-        labels_from_db = AdditionalDetails.objects.filter(is_active=True).filter(form__type='BOLO').values('id', 'display_name')
-        for labels in labels_from_db:
-            if str(labels['id']) == value:
-                value = labels['display_name']
-        print(value)
+        value = AdditionalDetails.objects.filter(form__type='BOLO').filter(id=value).values_list('display_name', flat=True)[0]
     return value
 
 
